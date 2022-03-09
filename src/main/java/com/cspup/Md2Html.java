d387fc588f38a316ebcbd67c0702a10de910fca3
cspup <yemuc@qq.com>
2022-03-09 17:33:20 +0800

    添加表格支持

--- src/main/java/com/cspup/Md2Html.java
+++ src/main/java/com/cspup/Md2Html.java
@@ -1,9 +1,12 @@
 package com.cspup;
 
+import com.vladsch.flexmark.ext.tables.TablesExtension;
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.ast.Node;
 import com.vladsch.flexmark.util.data.MutableDataSet;
+
+import java.util.List;
 
 /**
  * @author csp
@@ -13,6 +16,7 @@ import com.vladsch.flexmark.util.data.MutableDataSet;
 public class Md2Html {
     public String toHtml(String md) {
         MutableDataSet options = new MutableDataSet();
+        options.set(Parser.EXTENSIONS, List.of(TablesExtension.create()));
         Parser parser = Parser.builder(options).build();
         HtmlRenderer renderer = HtmlRenderer.builder(options).build();
 
